9a3eda2c04b28d3c61f7e7367b969c033f2339f1
mohan kumar <83080615+Kmohakumar@users.noreply.github.com>
2023-04-23 23:45:10 +0530

    removed grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './components/Home';
-import {Grid} from "@mui/material"
 import Header from './components/Header';
 import { Box } from '@mui/system';
 
