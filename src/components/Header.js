8804f0ac7f280cd85fc89c484df8df68a6bb6ee6
mohan kumar <83080615+Kmohakumar@users.noreply.github.com>
2023-04-24 09:51:31 +0530

    changed the colour

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 
 const Header = () => {
   return (
-    <Box sx={{ textAlign: 'center', background:"yellow" }}>
+    <Box sx={{ textAlign: 'center', background:"#FFD801" }}>
     <Typography variant="h6" sx={{ my: 2, fontWeight:"bold", fontSize:"2rem" }}>
       True Beacon
     </Typography>
